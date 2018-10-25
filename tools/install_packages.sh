4f52062df5fc11f621826bbf4c34cbdb51287684
Andrey Volkov <av903u@att.com>
2018-10-25 12:43:47 -0700

    miniMirror Helm chart
    This creates the initial miniMirror chart with the following k8s objects:
    - deployment
    - service
    - ingress
    
    This adds new tests to the airship-utils check gate related to Helm charts:
    - helm lint
    - helm package
    - helm install
    
    Change-Id: I6f5a3edcd66c3386fbab3b87c421b96f7198fdf1

--- tools/install_packages.sh
+++ tools/install_packages.sh
@@ -6,8 +6,8 @@ set -o xtrace
 
 apt update
 apt install -y curl
-curl -s localhost:8080/aptly_repo_signing.key | apt-key add -
-echo 'deb http://localhost:8080 xenial main' > /etc/apt/sources.list
+curl -s localhost:8889/aptly_repo_signing.key | apt-key add -
+echo 'deb http://localhost:8889 xenial main' > /etc/apt/sources.list
 apt-get update
 apt-cache policy accountsservice
 apt-get install -y accountsservice
